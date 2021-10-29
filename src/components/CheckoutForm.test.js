1bf2a8657567e927e0a58437ab80037fc35c3989
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-10-29 10:47:54 +0100

    I believe i am done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,21 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+//import MutationObserver from "mutationobserver-shim";
+import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", async () => {
+  render(<CheckoutForm />);
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", async () => {
+  render(<CheckoutForm />);
+  const submitButton = screen.getByRole("button");
+  userEvent.click(submitButton);
+
+  const successMessage = await screen.queryByTestId("successMessage");
+  expect(successMessage).toBeInTheDocument;
+});
