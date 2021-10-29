4afe3ae4d4e9d467ae5db51b2f4c339590583e1b
ModupeD <bimisoladaniel@Bimisolas-MacBook-Air.local[credential]>
2021-10-29 11:37:48 +0100

    I am done

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import MutationObserver from "mutationobserver-shim";
+import MutationObserver from "mutationobserver-shim";
 import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
@@ -13,9 +13,29 @@ test("renders without errors", async () => {
 
 test("shows success message on submit with form details", async () => {
   render(<CheckoutForm />);
+  const firstNameField = screen.getByLabelText(/first name*/i);
+  userEvent.type(firstNameField, "Modupeoluwa");
+
+  const lastNameField = screen.getByLabelText(/last name*/i);
+  userEvent.type(lastNameField, "Daniel");
+
+  const addressField = screen.getByLabelText(/address*/i);
+  userEvent.type(addressField, "Plot 1094");
+
+  const cityField = screen.getByLabelText(/city*/i);
+  userEvent.type(cityField, "Lagos");
+
+  const stateField = screen.getByLabelText(/state*/i);
+  userEvent.type(stateField, "Lagos");
+
+  const zipField = screen.getByLabelText(/zip/i);
+  userEvent.type(zipField, "45220");
+
   const submitButton = screen.getByRole("button");
   userEvent.click(submitButton);
 
-  const successMessage = await screen.queryByTestId("successMessage");
-  expect(successMessage).toBeInTheDocument;
+  await waitFor(() => {
+    const successMessage = screen.queryByTestId("successMessage");
+    expect(successMessage).toBeInTheDocument;
+  });
 });
